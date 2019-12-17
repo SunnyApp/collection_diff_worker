85ace201a69ba70613cb3a56712c527ec271a25e
ericmartineau <smartytime@gmail.com>
2019-12-17 14:20:02 -0700

    Initial checkin

---
+++ lib/collection_diff_isolate.dart
@@ -0,0 +1,4 @@
+library collection_diff_isolate;
+
+export 'diff_isolate.dart';
+export 'extensions.dart';
---
+++ lib/diff_isolate.dart
@@ -0,0 +1,24 @@
+import 'package:isolate_service/isolate_service.dart';
+
+IsolateService _diffRunner;
+
+IsolateService get diffRunner {
+  return _diffRunner ??= initializeDiffRunner((config) => config
+    ..poolSize = 2
+    ..defaultTimeout = Duration(seconds: 10)
+    ..failOnError = false
+    ..autoclose = true
+    ..debugName = "diffRunner");
+}
+
+set diffRunner(IsolateService runner) {
+  if (_diffRunner != null) {
+    _diffRunner.close();
+  }
+  _diffRunner = runner;
+}
+
+IsolateService initializeDiffRunner(void config(RunnerBuilder builder)) {
+  diffRunner = RunnerFactory.global.create(config);
+  return _diffRunner;
+}
